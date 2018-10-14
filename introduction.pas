674a10e2900e4c10adf08b28bca2cc9122cfa858
Liyu ZHANG <liyu.zhang@insa-rouen.fr>
2018-10-14 03:16:36 +0200

    LYZ : beautify introduction.pas

--- introduction.pas
+++ introduction.pas
@@ -1,82 +1,102 @@
-program intro;
 
-uses crt;
+Program intro;
 
-procedure introduction;
-begin
-clrscr;
-writeln('Welcome to the snake game!');
-writeln('This game has two mode which are one person mode and two peoson mode');
-writeln('For one person mode you can choose the difficulties easy and difficult');
-writeln('To control the snake');
-writeln('Press z to turn left');
-writeln('Press d to turn right');
-writeln('Press w to turn up');
-writeln('Press s to turn down');
-writeln('You are not allowed to touch the wall which will be shown as #');
-writeln('You should also try to eat as many beans as you can which is shown as $');
-end;
+Uses crt;
 
-function createfile():file;
-const
-path='D:\France\STPI2 S3\projet info\code\rank\';//the path of create a file
-var o:string;
-var fichier:file;
+Procedure introduction;
+Begin
+    clrscr;
+    writeln('Welcome to the snake game!');
+    writeln(
+          'This game has two mode which are one person mode and two peoson mode'
+    );
+    writeln(
+        'For one person mode you can choose the difficulties easy and difficult'
+    );
+    writeln('To control the snake');
+    writeln('Press z to turn left');
+    writeln('Press d to turn right');
+    writeln('Press w to turn up');
+    writeln('Press s to turn down');
+    writeln('You are not allowed to touch the wall which will be shown as #');
+    writeln(
+       'You should also try to eat as many beans as you can which is shown as $'
+    );
+End;
 
-begin
-assign(fichier,'rank.txt'); 
-rewrite(fichier);
-assign(fichier,'rank.txt'); 
-rewrite(fichier);
-o:=path+'rank.txt';
-assign(fichier,o);
-rewrite(fichier);
-close(fichier);
+Function createfile():   file;
 
-end;
+Const 
+    path =   'D:\France\STPI2 S3\projet info\code\rank\';
+    //the path of create a file
 
-procedure RecordTheScore(var fichier:file);
-const m=100;
-type classement= record
-name:string;
-score:array[1..m] of integer;
-end;
-var role:array[1..m] of classement;
-var i,j:integer;
-begin
-assign(fichier,'rank.txt');
-rewrite(fichier);
-for i:=1 to m do
-begin
-	writeln('Entez votre nom');
-	readln(role[i].name);
-	for j:=i to m do
-		read(role[i],score[j]);
-		readln;
-		write(fichier,role[i]);
-end;
-close(fichier);
-writeln;
-writeln;
-end;
+Var o:   string;
 
-procedure GiveTheRank(fichier:file);
-const m=100;
-type classement= record
-name:string;
-score:array[1..m] of integer;
-end;
-var role:array[1..m] of classement;
-var i,j:integer;
+Var fichier:   file;
 
-  
+Begin
+    assign(fichier,'rank.txt');
+    rewrite(fichier);
+    assign(fichier,'rank.txt');
+    rewrite(fichier);
+    o := path+'rank.txt';
+    assign(fichier,o);
+    rewrite(fichier);
+    close(fichier);
+
+End;
+
+Procedure RecordTheScore(Var fichier:File);
+
+Const m =   100;
+
+Type classement =   Record
+    name:   string;
+    score:   array[1..m] Of integer;
+End;
+
+Var role:   array[1..m] Of classement;
+
+Var i,j:   integer;
+Begin
+    assign(fichier,'rank.txt');
+    rewrite(fichier);
+    For i:=1 To m Do
+        Begin
+            writeln('Entez votre nom');
+            readln(role[i].name);
+            For j:=i To m Do
+                read(role[i],score[j]);
+            readln;
+            write(fichier,role[i]);
+        End;
+    close(fichier);
+    writeln;
+    writeln;
+End;
+
+Procedure GiveTheRank(fichier:File);
+
+Const m =   100;
+
+Type classement =   Record
+    name:   string;
+    score:   array[1..m] Of integer;
+End;
+
+Var role:   array[1..m] Of classement;
+
+Var i,j:   integer;
 
 
 
 
-var score:integer;
-var fichier:file;
-BEGIN
-introduction();
 
-end.
+
+Var score:   integer;
+
+Var fichier:   file;
+Begin
+    introduction();
+
+End.
