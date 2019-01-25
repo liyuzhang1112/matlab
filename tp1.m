0a3375c0531a8aed4fc4a37c2ec3dda5af982f56
liyuzhang1112 <46052875+liyuzhang1112@users.noreply.github.com>
2019-01-25 22:14:50 +0100

    Add files via upload

--- tp1.m
+++ tp1.m
@@ -1,8 +1,27 @@
+clear all;
+close all;
+%%
+%charger le fichier
 x = csvread('cardiac.dat',2,1);
+%%
+%Extraire les donnees
 c2 = x(:,2);
+%%
+%visualiser en texte
 str = num2str(c2);
-disp(str)
+disp(str);
+%%
 C2 = sort(c2);
 N = length(C2);
 subplot(2,1,1);plot(C2,(1:N)/N,'r');title('repartition');
-subplot(2,1,2);bar(c2,'r');
+%%
+first = min(c2);
+last = max(c2);
+intervalle = linspace(first,last,25);
+Num(1) = 0;
+for n = 2:25
+    n1 = find(c2 < intervalle(n));
+    n2 = find(c2 < intervalle(n-1));
+    Num(n) = length(n1)-length(n2);
+end
+subplot(2,1,2);bar(intervalle,Num);
